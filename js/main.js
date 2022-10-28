3f9341f23cf2a7068f834b4f5781c1a26df51d30
JuanCruz <jccarrea@gmail.com>
2022-10-28 14:52:25 -0300

    Se agrega modal y boton

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@ const openModal = document.querySelector('.main_cta');
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('modal_close');
 
-openModal.addEventListener('click', ()=>{
+openModal.addEventListener('click', (e)=>{
     e.preventDefault();
     modal.classList.add('modal--show');
-})
+    
+});
 
-closeModal.addEventListener('click', ()=>{
-    e.preventDefault();
+closeModal.addEventListener('click', (e)=>{
+    
     modal.classList.remove('modal--show');
-})
+});
